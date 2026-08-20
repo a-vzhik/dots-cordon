3226c7a0a95b7fd4b119a9644b3be3061faa30c6
Oleksandr <oleksandr.zhevzhyk@samsara.com>
2026-08-21 01:16:12 +0200

    Configure loglevel
    Added logger configuration which allows to enable/disable debug messages

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/a-vzhik/dots-cordon/engine"
 )
 
+func configureLogger(logLevel slog.Level) {
+	handler := slog.NewTextHandler(os.Stdout,
+		&slog.HandlerOptions{
+			Level: &logLevel,
+		})
+
+	slog.SetDefault(slog.New(handler))
+}
+
 func main() {
+	configureLogger(slog.LevelInfo)
+
 	players := []*engine.Player{
 		{
 			Score: 0,
@@ -21,8 +34,13 @@ func main() {
 		engine.NewGameField(20, 20),
 		players)
 
-	err1 := game.Move(0, 10, 9)
-	err2 := game.Move(1, 10, 10)
+	_ = game.Move(0, 10, 9)
+	_ = game.Move(1, 10, 10)
+	_ = game.Move(0, 9, 10)
+	_ = game.Move(1, 1, 1)
+	_ = game.Move(0, 10, 11)
+	_ = game.Move(1, 2, 2)
+	_ = game.Move(0, 11, 10)
 
-	log.Printf("The program running: %v %v", err1, err2)
+	slog.Info(fmt.Sprintf("Score %d : %d", game.Players[0].Score, game.Players[1].Score))
 }
