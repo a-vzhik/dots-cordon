447cd4a3708d2ae8c4ff11247ee339a994f2f038
Oleksandr <oleksandr.zhevzhyk@samsara.com>
2026-08-23 21:10:56 +0200

    Refactor GameField to a separate file
    No functional change. Moves GameField and tests for it out of
    game.go/game_test.go

---
+++ engine/gamefield.go
@@ -0,0 +1,69 @@
+package engine
+
+type GameField struct {
+	Width  uint8
+	Height uint8
+	Dots   [][]Dot
+}
+
+func NewGameField(width uint8, height uint8) *GameField {
+	dots := make([][]Dot, int(height))
+	for rowIdx := range dots {
+		row := make([]Dot, int(width))
+		for colIdx := range row {
+			row[colIdx] = Dot{
+				Col: uint8(colIdx),
+				Row: uint8(rowIdx),
+			}
+		}
+
+		dots[rowIdx] = row
+	}
+
+	return &GameField{
+		Width:  width,
+		Height: height,
+		Dots:   dots,
+	}
+}
+
+func (gf *GameField) Transform(row uint8, col uint8, applyFunc func(Dot) Dot) {
+	oldDot := gf.Dots[row][col]
+	gf.Dots[row][col] = applyFunc(oldDot)
+}
+
+func (gf *GameField) TransformMany(sourceDots []Dot, applyFunc func(Dot) Dot) {
+	for _, source := range sourceDots {
+		gf.Dots[source.Row][source.Col] = applyFunc(source)
+	}
+}
+
+func (gf *GameField) TransformFunc(filterFunc func(Dot) bool, applyFunc func(Dot) Dot) {
+	for rowIdx, row := range gf.Dots {
+		for colIdx, oldDot := range row {
+
+			if !filterFunc(oldDot) {
+				continue
+			}
+
+			newDot := applyFunc(oldDot)
+			gf.Dots[rowIdx][colIdx] = newDot
+		}
+	}
+}
+
+func (gf *GameField) Find(filterFunc func(Dot) bool) []Dot {
+	filtered := make([]Dot, 0)
+	for _, row := range gf.Dots {
+		for _, dot := range row {
+
+			if !filterFunc(dot) {
+				continue
+			}
+
+			filtered = append(filtered, dot)
+
+		}
+	}
+	return filtered
+}
---
+++ engine/gamefield_test.go
@@ -0,0 +1,91 @@
+package engine
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGameFieldTransformFiltersDots(t *testing.T) {
+	t.Run("single dot", func(t *testing.T) {
+		field := NewGameField(3, 2)
+
+		field.TransformFunc(
+			func(dot Dot) bool {
+				return dot.Row == 1 && dot.Col == 2
+			},
+			func(dot Dot) Dot {
+				return dot.WithOwner(PlayerIndex(1))
+			},
+		)
+
+		want := NewGameField(3, 2)
+		want.Dots[1][2] = want.Dots[1][2].WithOwner(PlayerIndex(1))
+		assert.Equal(t, want, field)
+	})
+
+	t.Run("group of dots", func(t *testing.T) {
+		field := NewGameField(3, 3)
+
+		field.TransformFunc(
+			func(dot Dot) bool {
+				return dot.Row == 1
+			},
+			func(dot Dot) Dot {
+				return dot.WithKilled()
+			},
+		)
+
+		want := NewGameField(3, 3)
+		for colIdx, dot := range want.Dots[1] {
+			want.Dots[1][colIdx] = dot.WithKilled()
+		}
+		assert.Equal(t, want, field)
+	})
+}
+
+func TestGameFieldFind(t *testing.T) {
+	tests := []struct {
+		name   string
+		filter func(Dot) bool
+		want   []Dot
+	}{
+		{
+			name: "nothing",
+			filter: func(Dot) bool {
+				return false
+			},
+			want: []Dot{},
+		},
+		{
+			name: "one dot",
+			filter: func(dot Dot) bool {
+				return dot.Row == 1 && dot.Col == 2
+			},
+			want: []Dot{
+				{Col: 2, Row: 1},
+			},
+		},
+		{
+			name: "multiple dots",
+			filter: func(dot Dot) bool {
+				return dot.Row == 1
+			},
+			want: []Dot{
+				{Col: 0, Row: 1},
+				{Col: 1, Row: 1},
+				{Col: 2, Row: 1},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			field := NewGameField(3, 2)
+
+			got := field.Find(tt.filter)
+
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
